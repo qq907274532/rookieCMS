1a79747c222d77315889f3d547f1211db28138b1
svip_zx <907274532@qq.com>
2017-04-26 23:28:55 +0800

    忽略upload

---
+++ App/Manager/Controller/BrandController.class.php
@@ -0,0 +1,15 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Administrator
+     * Date: 2017/4/26
+     * Time: 21:58
+     */
+
+    namespace Manager\Controller;
+
+
+    class BrandController
+    {
+
+    }
---
+++ App/Manager/Controller/GoodsController.class.php
@@ -0,0 +1,15 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Administrator
+     * Date: 2017/4/26
+     * Time: 21:09
+     */
+
+    namespace Manager\Controller;
+
+
+    class GoodsController
+    {
+
+    }
